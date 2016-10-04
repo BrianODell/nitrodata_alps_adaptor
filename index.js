c5b2a43f1534b7d6d87b5b514c7bfd1844404d71
Brian O'Dell <brian.odell@nextgearcapital.com>
2016-10-04 12:41:07 -0400

    pulled alps_adaptor.updateInstance() from internal repo

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ var Promise = require('bluebird'),
     generateAllModels = require('./lib/generate_all_models'),
     connector = require('./lib/connector'),
     getRandom = require('./lib/getRandom'),
-    insert = require('./lib/insert').insert;
-    findRecords = require('./lib/find_records');
+    insert = require('./lib/insert').insert,
+    findRecords = require('./lib/find_records'),
+    updateRecords = require('./lib/update_records');
 
 module.exports = {
     generateAllModels: generateAllModels,
     init: connector.init,
     getRandom: getRandom,
     findRecords: findRecords,
-    updateRecords: null,
+    updateRecords: updateRecords,
     deleteRecords: null,
     insertRecord: insert
 }
